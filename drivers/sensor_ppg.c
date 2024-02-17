46a59c66652f5548b44145e53987883f6616da59
Mario Lanzilotta <mariolanzilotta@protonmail.com>
2024-02-17 18:12:02 -0500

    Implemented more SPI configs

--- drivers/sensor_ppg.c
+++ drivers/sensor_ppg.c
@@ -45,7 +45,7 @@ int ppg_start_config(const struct device *dev) {
 
 int ppg_config_sampling_freq(const struct device *dev, uint16_t freq) {
     int err = 0;
-    
+
     //Set sampling frequency
     spi_reg = 0x12;
     spi_rw = 0x1;
@@ -61,16 +61,60 @@ int ppg_config_sampling_freq(const struct device *dev, uint16_t freq) {
 
 int ppg_config_num_channels(const struct device *dev) {
     int err = 0;
+
+    spi_reg = 0x37;
+    spi_rw = 0x1;
+    spi_value = 0xfc00; 
+    spi_cmd = spi_reg + (spi_rw << 7) + (spi_value << 8);
+	err = spi_write(dev, &spi_cfg, &tx_bufs);
+    if (err < 0) {
+			printk("SPI Write failed (%d)\n", err);
+			return err;
+	}
+    return err;
+
+    spi_reg = 0x3c;
+    spi_rw = 0x1;
+    spi_value = 0x31c6; 
+    spi_cmd = spi_reg + (spi_rw << 7) + (spi_value << 8);
+	err = spi_write(dev, &spi_cfg, &tx_bufs);
+    if (err < 0) {
+			printk("SPI Write failed (%d)\n", err);
+			return err;
+	}
     return err;
 }
 
 int ppg_config_fifo(const struct device *dev) { 
     int err = 0;
+    //Enable Data hold for slot A and B
+    spi_reg = 0x5f;
+    spi_rw = 0x1;
+    spi_value = 0x0006;
+    spi_cmd = spi_reg + (spi_rw << 7) + (spi_value << 8);
+	err = spi_write(dev, &spi_cfg, &tx_bufs);
+    if (err < 0) {
+			printk("SPI Write failed (%d)\n", err);
+			return err;
+	}
     return err;
 }
 
 int ppg_config_leds(const struct device *dev) {
     int err = 0;
+
+    //Set which LEDs pulse
+    spi_reg = 0x14;
+    spi_rw = 0x1;
+    spi_value = 0x054f;
+    spi_cmd = spi_reg + (spi_rw << 7) + (spi_value << 8);
+	err = spi_write(dev, &spi_cfg, &tx_bufs);
+    if (err < 0) {
+			printk("SPI Write failed (%d)\n", err);
+			return err;
+	}
+
+
     return err;
 }
 
@@ -93,6 +137,11 @@ int ppg_exit_config(const struct device *dev) {
 
 int ppg_read_sensors(const struct device *dev, const struct device *dev2, uint16_t num_samples) {
     int err = 0;
+    spi_reg = 0x60;
+    spi_rw = 0x0;
+    spi_value = 0x0000; 
+    spi_cmd = spi_reg + (spi_rw << 7) + (spi_value << 8);
+    err = spi_write(dev, &spi_cfg, &tx_bufs);
     err = spi_read(dev, &spi_cfg, &rx_bufs);
     if (err < 0) {
 			printk("SPI Read failed (%d)\n", err);
